a625d99819a533c0719dfb240fcb267a2f75da19
jaedkim23 <38264272+jaedkim23@users.noreply.github.com>
2026-08-18 14:42:51 -0700

    Add files via upload

---
+++ turnkey/html/nsfproject/assets/nsfproject/footer/regular-footer.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PHPStorm
+ * User: bteague
+ * Date: 3/13/26
+ * Time: 3:39 PM
+ * PHP Version: 7.4+
+ *
+ * @category
+ * @package
+ * @author   Bryan Teague <bryant@sandiego.edu>
+ * @license  https://github.sandiego.edu.com/ GPL
+ * @link     https://github.sandiego.edu.com/
+ **/
+?>
+</div>
+<footer class="site-footer">
+	<div class="site-footer__inner">
+		<img src="logo" alt="schoolName">
+
+		MYRESOURCELINKS
+
+		<p class="site-footer__copy">
+			©<?php
+			echo date("Y"); // Outputs: 2026
+			?> schoolName. All rights reserved.
+		</p>
+	</div>
+</footer>
+
+</body>
+</html>
---
+++ turnkey/html/nsfproject/assets/nsfproject/footer/setup-footer.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PHPStorm
+ * User: bteague
+ * Date: 3/13/26
+ * Time: 3:39 PM
+ * PHP Version: 7.4+
+ *
+ * @category
+ * @package
+ * @author   Bryan Teague <bryant@sandiego.edu>
+ * @license  https://github.sandiego.edu.com/ GPL
+ * @link     https://github.sandiego.edu.com/
+ **/
+?>
+<footer>
+   <div> © 2026 UNIVERSITY OF SAN DIEGO. ALL RIGHTS RESERVED.
+   </div>
+</footer>
+</body>
+</html>
